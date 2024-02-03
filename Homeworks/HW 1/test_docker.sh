fff2615708eda2a3757cb0ba2286522d6e3a9c33
Aditya Shrivastava <89076159+AdityaShrivastava30@users.noreply.github.com>
2024-02-03 14:19:54 -0800

    uploading docker test

---
+++ Homeworks/HW 1/test_docker.sh
@@ -0,0 +1,11 @@
+
+#! /bin/bash
+#
+
+PATH_CPU_TEST="C:/Program Files/CC/cpu-tests.sh"
+FILEIOTEST_PATH="C:/Program Files/CC/test-File-io.sh"
+MEMORYTEST_PATH="C:/Program Files/CC/test-Memory.sh"
+
+"$PATH_CPU_TEST"
+"$FILEIOTEST_PATH"
+"$MEMORYTEST_PATH"
