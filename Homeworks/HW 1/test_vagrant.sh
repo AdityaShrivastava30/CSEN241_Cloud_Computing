e224970270fe9bc71c433d261d9f59024e5b9e7b
Aditya Shrivastava <89076159+AdityaShrivastava30@users.noreply.github.com>
2024-02-03 14:15:37 -0800

    uploaded vagrant_test.sh

---
+++ Homeworks/HW 1/test_vagrant.sh
@@ -0,0 +1,17 @@
+
+#! /bin/bash
+#
+
+PATH_CPU_TEST="C:/Program Files/CC/cpu-tests.sh"
+FILEIOTEST_PATH="C:/Program Files/CC/test-File-io.sh"
+MEMORYTEST_PATH="C:/Program Files/CC/test-Memory.sh"
+
+sudo apt-get update
+sudo apt-get install -y sysbench
+chmod +x "C:/Program Files/CC/cpu-tests.sh"
+chmod +x "C:/Program Files/CC/test-File-io.sh"
+chmod +x "C:/Program Files/CC/test-Memory.sh"
+
+"$PATH_CPU_TEST"
+"$FILEIOTEST_PATH"
+"$MEMORYTEST_PATH"
